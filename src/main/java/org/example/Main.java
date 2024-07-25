68885b62f4f8c23516ebec1498c161f39c0393ac
Ritish Kandel <s3947204@student.rmit.edu.au>
2024-07-25 17:49:45 +1000

    Added bubble sort method to sort an arraylist of Integers using bubble sort.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<Integer> arrayToSort = new ArrayList<Integer>();
+        arrayToSort.add(7);
+        arrayToSort.add(2);
+        arrayToSort.add(76);
+        arrayToSort.add(4);
+        arrayToSort.add(99);
+        bubbleSort(arrayToSort);
+        for (int i = 0; i < arrayToSort.size(); i++){
+            System.out.println(arrayToSort.get(i));
+        }
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static void bubbleSort(ArrayList<Integer> arrayToSort){
+        for(int i = 0; i < arrayToSort.size(); i++){
+            for(int j = 0; j < arrayToSort.size() - i - 1; j++){
+                if(arrayToSort.get(j) > arrayToSort.get(j+1)){
+                    Integer temp = arrayToSort.get(j);
+                    arrayToSort.set(j, arrayToSort.get(j+1));
+                    arrayToSort.set(j+1, temp);
+                }
+            }
         }
     }
 }
